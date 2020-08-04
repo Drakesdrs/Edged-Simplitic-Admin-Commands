e8e12d622f70ecf9eaf9659c30a9acdb43a4a298
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-08-04 19:17:02 +0200

    Update TalvekiansAdmin.js

--- TalvekiansAdmin.js
+++ TalvekiansAdmin.js
@@ -1,17 +1,42 @@
+const vipitem = 174778 // Replace this with your item ID
+
+const colorcodeRegex = /\\c[0-9]/g
+const hexcodeRegex = /(\[#[0-9a-fA-F]{6}\])/g
+const internalcodeRegex = /<color:[0-9A-F]{6}>/g
+
+vips = [] // array of vip userids
+
+Game.on("playerJoin", async(p) => {
+	let hasVIP = await p.ownsAsset(vipitem)
+	if (hasVIP) {
+		vips.push(p.userId)
+	}
+})
+
+
+
+
 Game.on("chat", async (player, message) => {
-    if (player.admin){
-        Game.messageAll("[BH Admin Abuser] "+ player.username+ ": "+ message)
+    if (player.admin && hasVIP){
+        Game.messageAll("[BH Admin Abuser][Donator] "+ player.username+ ":[#ffffff] "+ message)
+	}
+	if (player.admin){
+        Game.messageAll("[BH Admin Abuser] "+ player.username+ ":[#ffffff] "+ message)
     }
 
-    else if (player.username == "Talveka"){
-        Game.messageAll("[Queen] "+ player.username+": "+message)
+    else if (player.username == "Talveka" && hasVIP){
+        Game.messageAll("[#00a2fa][Queen] "+ player.username+":[#ffffff] "+message)
 
     }
     else if (player.username == "Edge."){
-        Game.messageAll("[Developer] "+ player.username+": "+message)
+        Game.messageAll("[#ff0000][Developer] "+ player.username+":[#ffffff] "+message)
 
     }
-    else {
-        Game.messageAll("[Simp] "+player.username+": "+message)
-    }
+    else if (hasVIP){
+        Game.messageAll("[#fffb00][Simp][Donator] "+player.username+":[#ffffff] "+message)
+	}
+	else{
+		Game.messageAll("[#fffb00][Simp] "+player.username+":[#ffffff] "+message)
+
+	}
 });
